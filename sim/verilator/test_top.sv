5959b2444f433ff8cd44cf66750fe5f0ff177131
yasei <arazdakov@gmail.com>
2026-08-11 01:29:43 +0300

    feat: add lsu and data_mem to test_top

--- sim/verilator/test_top.sv
+++ sim/verilator/test_top.sv
@@ -9,18 +9,37 @@ module test_top (
   logic [31:0] instr;
   logic        core_stall;
 
+  logic        core_req;
+  logic        core_we;
+  logic [ 2:0] core_size;
+  logic [31:0] core_addr;
+  logic [31:0] core_wd;
+  logic [31:0] core_rd;
+
+  logic [31:0] mem_addr;
+  logic        mem_we;
+  logic [31:0] mem_wd;
+  logic [ 3:0] mem_be;
+  logic        mem_req;
+  logic        ready;
+
+  logic [31:0] lsu_rd;
+  logic [31:0] mem_rd;
+
+  assign ready = 1'b1;
+
   processor_core core (
       .clk_i       (clk_i),
       .rst_i       (rst_i),
       .stall_i     (core_stall),
       .instr_i     (instr),
-      .mem_rd_i    (32'b0),
+      .mem_rd_i    (core_rd),
       .instr_addr_o(instr_addr),
-      .mem_addr_o  (),
-      .mem_size_o  (),
-      .mem_req_o   (),
-      .mem_we_o    (),
-      .mem_wd_o    (),
+      .mem_addr_o  (core_addr),
+      .mem_size_o  (core_size),
+      .mem_req_o   (core_req),
+      .mem_we_o    (core_we),
+      .mem_wd_o    (core_wd),
       .irq_req_i   (irq_req_i),
       .irq_ret_o   (irq_ret_o)
   );
@@ -28,6 +47,36 @@ module test_top (
   instr_mem imem (
       .read_addr_i(instr_addr),
       .read_data_o(instr)
+  );
+
+  lsu lsu_inst (
+      .clk_i(clk_i),
+      .rst_i(rst_i),
+      .core_req_i(core_req),
+      .core_we_i(core_we),
+      .core_size_i(core_size),
+      .core_addr_i(core_addr),
+      .core_wd_i(core_wd),
+      .core_rd_o(core_rd),
+      .core_stall_o(core_stall),
+      .mem_req_o(mem_req),
+      .mem_we_o(mem_we),
+      .mem_be_o(mem_be),
+      .mem_addr_o(mem_addr),
+      .mem_wd_o(mem_wd),
+      .mem_rd_i(lsu_rd),
+      .mem_ready_i(ready)
+  );
+
+  data_mem data_mem (
+      .clk_i(clk_i),
+      .mem_req_i(mem_req),
+      .write_enable_i(mem_we),
+      .byte_enable_i(mem_be),
+      .addr_i(mem_addr),
+      .write_data_i(mem_wd),
+      .read_data_o(mem_rd),
+      .ready_o()
   );
 
 endmodule
